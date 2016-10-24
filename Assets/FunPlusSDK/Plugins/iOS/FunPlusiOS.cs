5493010d47e97100d7f0c207e978f36ca4e73f09
yuankun.zhang <fmcocoa@gmail.com>
2016-10-24 19:23:58 +0800

    iOS

---
+++ Assets/FunPlusSDK/Plugins/iOS/FunPlusiOS.cs
@@ -0,0 +1,23 @@
+﻿//#if UNITY_IPHONE
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using HSMiniJSON;
+
+namespace FunPlusSDK
+{
+	public class FunPlusiOS
+	{
+		[DllImport ("__Internal")]
+		private static extern void fpInstall (string appId, string appKey, string environment);
+
+		public FunPlusiOS () {}
+
+		public void install (string appId, string appKey, string environment) {
+			install(appId, appKey, environment);
+		}
+	}
+
+}
+//#endif
