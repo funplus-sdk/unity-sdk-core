d6bc90bed851508e57f9df835954c32b88c390b9
yuankun.zhang <fmcocoa@gmail.com>
2016-11-16 14:59:15 +0800

    4.0.1-alpha.1

--- Assets/FunPlusSDK/Plugins/FunPlusSDK.cs
+++ Assets/FunPlusSDK/Plugins/FunPlusSDK.cs
@@ -10,7 +10,7 @@ namespace FunPlus
 	{
 		#region FunPlusSDK members
 
-		public static string VERSION = "4.0.1-alpha.0";
+		public static string VERSION = "4.0.1-alpha.1";
 
 		#if UNITY_IOS || UNITY_ANDROID
 		private static FunPlusSDK instance = null;
