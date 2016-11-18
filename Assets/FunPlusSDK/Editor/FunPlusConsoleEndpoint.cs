59d80a704648ee5c77f575dcc89bbf316d208531
yuankun.zhang <fmcocoa@gmail.com>
2016-11-18 17:40:39 +0800

    build.sh

---
+++ Assets/FunPlusSDK/Editor/FunPlusConsoleEndpoint.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using FunPlus;
+
+public class FunPlusConsoleEndpoint
+{
+	private const string FunPlusPath = "Assets/FunPlusSDK/";
+
+	public static void ExportPackage()
+	{
+		Debug.Log("Exporting FunPlus SDK Unity Package...");
+		string path = OutputPath;
+		Debug.Log ("XXXXX " + path);
+
+		try
+		{
+			if (!File.Exists(Path.Combine(Application.dataPath, "Temp")))
+			{
+				AssetDatabase.CreateFolder("Assets", "Temp");
+			}
+
+			AssetDatabase.MoveAsset(FunPlusPath + "Resources/FunPlusSDKConfig.asset", "Assets/Temp/FunPlusSDKConfig.asset");
+
+			string[] files = (string[])Directory.GetFiles(FunPlusPath, "*.*", SearchOption.AllDirectories);
+
+			AssetDatabase.ExportPackage(
+				files,
+				path,
+				ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Recurse);
+		}
+		finally
+		{
+			// Move files back no matter what
+			AssetDatabase.MoveAsset("Assets/Temp/FunPlusSDKConfig.asset", FunPlusPath + "Resources/FunPlusSDKConfig.asset");
+			AssetDatabase.DeleteAsset("Assets/Temp");
+		}
+
+		Debug.Log("Finished exporting!");
+	}
+
+	private static string OutputPath
+	{
+		get
+		{
+			string projectRoot = Directory.GetCurrentDirectory();
+			var outputDirectory = new DirectoryInfo(Path.Combine(projectRoot, "Release"));
+
+			// Create the directory if it doesn't exist
+			outputDirectory.Create();
+
+			string packageName = string.Format("funplus-unity-sdk-{0}.unitypackage", FunPlusSDK.VERSION);
+			return Path.Combine(outputDirectory.FullName, packageName);
+		}
+	}
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,17 +3,19 @@
 ### v4.0.1-alpha.2
 
 * `TraceCustomEventWithNameAndProperties()`
+* Upgrade native iOS SDK to v4.0.3-alpha.1
+* Upgrade native Android SDK to v4.0.1-alpha.1
 
 ### v4.0.1-alpha.1
 
-* upgrade native Android SDK to v4.0.1-alpha.0
+* Upgrade native Android SDK to v4.0.1-alpha.0
 * Remove the funsdk-default-config.json file.
 * Modify post process script to fit the new Android SDK.
 * Clean up some XUPorter warnings.
 
 ### v4.0.1-alpha.0
 
-* upgrade native iOS SDK to v4.0.3-alpha.0
+* Upgrade native iOS SDK to v4.0.3-alpha.0
 * API to get session ID.
 * FunPlus ID module ready to work.
 * Remove the funsdk-default-config.plist file.
